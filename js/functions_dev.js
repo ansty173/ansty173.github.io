d14ec3e1cd776c51e7cf7c9de966d1deafb9b4ba
iou2much <iou2much@sina.com>
2016-12-10 02:53:47 +0800

    finish

--- js/functions_dev.js
+++ js/functions_dev.js
@@ -98,23 +98,22 @@ function timeElapse(date){
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		//hours = "0" + hours;
-		hours =  hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		//minutes = "0" + minutes;
-		minutes =  minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		//seconds = "0" + seconds;
-		seconds =  seconds;
-	}
         if(hours<0){
-hours=0;minutes=0;seconds=0;
+days=0;hours=0;minutes=0;seconds=0;
+	}else{
+	    if (hours < 10) {
+	    	hours = "0" + hours;
+	    }
+	    seconds = seconds % 3600;
+	    var minutes = Math.floor(seconds / 60);
+	    if (minutes < 10) {
+	    	minutes = "0" + minutes;
+	    }
+	    seconds = seconds % 60;
+	    if (seconds < 10) {
+	    	seconds = "0" + seconds;
+	    }
+
 	}
 	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
 	$("#elapseClock").html(result);
